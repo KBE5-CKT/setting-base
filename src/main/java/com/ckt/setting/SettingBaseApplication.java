6cf4b71dbc63cbcd620d9cf48d16ee7d8bac844f
maximum0 <maximum.zero95@gmail.com>
2025-05-20 09:35:36 +0900

    chore: 프로젝트 기본 설정 및 환경 변수 설정
    - 새로운 스프링 부트 프로젝트의 기본 설정
    - 환경 분리 작업 (local, prod)
    - JPA 및 MySQL 연동을 위한 의존성 및 기본적인 설정
    - actuator 추가로 환경별 DB 초기화 확인

---
+++ src/main/java/com/ckt/setting/SettingBaseApplication.java
@@ -0,0 +1,13 @@
+package com.ckt.setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ckt/setting/SettingBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckt.setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
